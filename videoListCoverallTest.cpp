127770d7789b09ead08154158b4f8d91f2e4ef76
jgutierrezCSU <jgutierrez@mail.csuchico.edu>
2021-05-20 14:24:52 -0700

    created videoListTest constructor

---
+++ videoListCoverallTest.cpp
@@ -0,0 +1,16 @@
+/**
+  Unit Tests for Video List Repository
+**/
+
+#include <gtest/gtest.h>
+#include "video.h"
+#include "vlist.h"
+ 
+class VideoListTest : public ::testing::Test
+{
+	protected:
+		videoListTest(){} //constructor runs before each test
+		virtual ~videoListTest(){} //destructor cleans up after tests
+		virtual void SetUp(){} //sets up before each test (after constructor)
+		virtual void TearDown(){} //clean up after each test, (before destructor) 
+};
